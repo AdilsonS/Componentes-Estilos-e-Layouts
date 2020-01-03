4316486cfc0ec63c87dd92ab67fdaef26d5ab5f2
Adilson Souza <adilsonsouzadocarmojf@hotmail.com>
2020-01-03 17:58:25 -0300

    Criado a tela de jogo Criado navegação entre as telas

--- App.js
+++ App.js
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 import Header from "./components/Header";
 import StartGameScreen from './sreens/StartGameScreen';
+import GameScreen from './sreens/GameSreen';
 
 export default function App() {
+  const [userNumber, setUserNumber] = useState();
+
+  const startGameHandler = selectedNumber => {
+    console.log('eu');
+    setUserNumber(selectedNumber);
+  }
+
+  let content = <StartGameScreen onStartGame={startGameHandler} />
+  if (userNumber){
+    content = <GameScreen userChoice={userNumber} />
+  }
+
   return (
     <View style={styles.screen}>
-      <Header title="Guess a Number"/>
-      <StartGameScreen/>
+      <Header title="Guess a Number" />
+      {content}
     </View>
   );
 }
