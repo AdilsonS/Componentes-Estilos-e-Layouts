ec8157681b2cbb42315aab3b04988916252ed859
Adilson Souza <adilsonsouzadocarmojf@hotmail.com>
2020-01-09 10:51:58 -0300

    Aplicando e estilizando ScrollView

--- sreens/GameSreen.js
+++ sreens/GameSreen.js
@@ -1,5 +1,5 @@
 import React, { useState, useRef, useEffect } from 'react';
-import { View, Text, StyleSheet, Alert } from 'react-native';
+import { View, Text, StyleSheet, Alert, ScrollView } from 'react-native';
 
 import Card from '../components/Card';
 import Colors from '../constants/colors';
@@ -16,10 +16,18 @@ const generateRandom = (min, max, exlude) => {
     return rndNum;
 }
 
-const GameScreen = props => {
-  const [currentGuess, setCurrentGuess] = useState(generateRandom(1, 100, props.userChoice));
-  const [rounds, setRounds] = useState(0);
+const renderListItem = (numberOfRounds, value) => (
+  <View style={styles.listItem} key={value}>
+    <Text>#{numberOfRounds}</Text>
+    <Text>{value}</Text>
+  </View>
+);
 
+const GameScreen = props => {
+  const initialGuess = generateRandom(1, 100, props.userChoice)
+  const [currentGuess, setCurrentGuess] = useState(initialGuess);
+  //const [rounds, setRounds] = useState(0);
+  const [pastGuesses, setPastGuesses] = useState([initialGuess]);
   const currentLow = useRef(1);
   const currentHigh = useRef(100);
 
@@ -27,7 +35,7 @@ const GameScreen = props => {
 
   useEffect(() => {
     if (currentGuess === userChoice) {
-      onGameOver(rounds);
+      onGameOver(pastGuesses.length);
     }
   }, [currentGuess, userChoice, onGameOver])
 
@@ -41,13 +49,15 @@ const GameScreen = props => {
     if (direction === "lower")
       currentHigh.current = currentGuess
     else if (direction === "greater")
-      currentLow.current = currentGuess;
+      currentLow.current = currentGuess + 1;
     else
       return;
 
     const nextNumber = generateRandom(currentLow.current, currentHigh.current, currentGuess);
     setCurrentGuess(nextNumber);
-    setRounds(curRounds => curRounds + 1);
+    //setRounds(curRounds => curRounds + 1);
+    setPastGuesses(curPastGuest => [nextNumber, ...curPastGuest]);
+
   }
 
   return (
@@ -60,6 +70,12 @@ const GameScreen = props => {
           <Text style={styles.arrow} onPress={nextGuessHandler.bind(this, 'lower')}>V</Text>
         </View>
       </Card>
+
+      <View style={styles.listContainer}>
+        <ScrollView contentContainerStyle={styles.list}>
+          {pastGuesses.map((guess, index) => renderListItem(pastGuesses.length - index, guess))}
+        </ScrollView>
+      </View>
     </View>
 
   )
@@ -88,6 +104,29 @@ const styles = StyleSheet.create({
   rotate: {
     backgroundColor: Colors.acent,
     transform: [{ rotate: '-180deg' }]
+  },
+
+  listContainer: {
+    flex:1,
+    width: '80%',
+    marginTop:'5%',
+  },
+
+  list:{
+    flexGrow:1,
+    justifyContent:'flex-end',
+    alignItems:'center'
+  },
+
+  listItem: {
+    borderColor: Colors.black,
+    borderWidth: 1,
+    padding: 15,
+    marginVertical: 10,
+    backgroundColor: Colors.white,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    width:'85%',
   }
 
 });
